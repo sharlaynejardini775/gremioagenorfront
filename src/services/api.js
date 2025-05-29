da9b5e35308b0c5ddbed592e24d20de039e5a631
sharlaynejardini775 <sharlayne.fonseca@professor.barueri.br>
2025-05-29 15:03:26 -0300

    2905

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://gremioagenorback-1.onrender.com/', // ou a URL da sua API
+  baseURL: 'https://gremioagenorback.onrender.com/', // ou a URL da sua API
 });
 
 export default api;
