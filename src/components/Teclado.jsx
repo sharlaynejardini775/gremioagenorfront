830cdfc39f1e4eeb43c749e671a2652e1f5db40b
sharlaynejardini775 <sharlayne.fonseca@professor.barueri.br>
2025-06-02 10:28:13 -0300

    0206

--- src/components/Teclado.jsx
+++ src/components/Teclado.jsx
@@ -3,6 +3,9 @@ import './Teclado.css';
 
 const Teclado = ({ onNumero, onBranco, onCorrige, onConfirma, disabled }) => {
   const botoes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+  
+  // Lista dos números que devem estar desabilitados
+  const numerosDesabilitados = ['4', '5', '6', '7', '8', '9', '0'];
 
   return (
     <div className="teclado-container">
@@ -12,7 +15,8 @@ const Teclado = ({ onNumero, onBranco, onCorrige, onConfirma, disabled }) => {
             key={num} 
             className="tecla" 
             onClick={() => onNumero(num)}
-            disabled={disabled}
+            // Desabilita se o número está na lista OU se o teclado geral está desabilitado
+            disabled={numerosDesabilitados.includes(num) || disabled}
           >
             {num}
           </button>
